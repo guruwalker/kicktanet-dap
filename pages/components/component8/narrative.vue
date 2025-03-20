4f083b4bf1312d4f9c91091acf5c7a1f109d3848
innocent-maina <innocentwahome@gmail.com>
2025-03-21 00:54:41 +0300

    add components

---
+++ pages/components/component8/narrative.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Team from "~~/components/Data/Team";
+
+useHead({
+  bodyAttrs: {
+    class: "navbar-dark navbar-dark-2",
+  },
+});
+
+const teamMembers = ref(Team);
+</script>
+
+<template>
+  <section
+    id="team-2"
+    class="gr--whitesmoke pb-40 inner-page-hero team-section"
+  >
+    <div class="container">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col">
+            <div class="inner-page-title text-center">
+              <h2 class="s-52 w-700">Component 1 - Narrative</h2>
+              <p class="p-lg">lorem8</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
